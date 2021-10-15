2b15a4dceffda0810ad46f2a250af497dc5f45d2
codren <tkdrms0291@naver.com>
2021-10-15 21:09:20 +0900

    Auditing

--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name = "member")
 @Getter @Setter
 @ToString
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @Column(name = "member_id")
@@ -36,9 +36,8 @@ public class Member {
         member.setAddress(joinFormDto.getAddress());
         String password = passwordEncoder.encode(joinFormDto.getPassword());
         member.setPassword(password);
-        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN);
         return member;
     }
-
 }
 
